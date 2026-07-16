7fb2814bafd86dba3f6469c0af1569703f6867fa
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-07-16 23:23:34 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: 
+    ["@xenova/transformers"],
 };
 
 export default nextConfig;
