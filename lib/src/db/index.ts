2f361c3c45f594c9f200da6a8cf3c58533d5a943
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-07-28 20:17:29 +0100

    Create index.ts

---
+++ lib/src/db/index.ts
@@ -0,0 +1,6 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
+
+const client = neon(process.env.DATABASE_URL!);
+export const db = drizzle(client, { schema });
